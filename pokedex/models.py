6f239773c70181a734cb3edba377297a3caeab1e
laboratorio.fs <jhelan.basantes@uisek.edu.ec>
2026-05-05 10:47:42 -0500

    Laboratorio 4 Terminado

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    type = models.CharField(max_length=50, null=False)
+    wight = models.FloatField()
+    height = models.FloatField()
+    
+    def __str__(self):
+        return self.name
