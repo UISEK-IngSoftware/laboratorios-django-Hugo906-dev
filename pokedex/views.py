42f373bba0090e8d90c77502716998ff2ab159c0
laboratorio.fs <jhelan.basantes@uisek.edu.ec>
2026-05-12 10:18:29 -0500

    Laboratorio 5 Extendido

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,15 +1,19 @@
+from multiprocessing import context
+
 from django.http import HttpResponse
 from django.template import loader
 from .models import Pokemon, Trainer
+from django.shortcuts import redirect, render
+from pokedex.forms import PokemonForm
 
 def index(request):
     pokemons = Pokemon.objects.all() #Select * from pokedex_pokemon
     trainers = Trainer.objects.all() #Select * from pokedex_trainer
     template = loader.get_template('index.html')
-    return HttpResponse(template.render({
+    return render(request, 'index.html', {
         'pokemons': pokemons,
         'trainers': trainers
-    }, request))
+    })
 
 def pokemon(request, id: int):
     pokemon = Pokemon.objects.get(id=id) #select * from pokedex_pokemon where id = id
@@ -26,3 +30,29 @@ def trainer(request, id: int):
         'trainer': trainer
     }
     return HttpResponse(template.render(context, request))
+
+def add_pokemon(request):
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm()
+    return render(request, 'pokemon_form.html', {'form': form})
+
+def edit_pokemon(request, id: int):
+    pokemon = Pokemon.objects.get(id=id)
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, request.FILES, instance=pokemon)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm(instance=pokemon)
+    return render(request, 'pokemon_form.html', {'form': form})
+
+def delete_pokemon(request, id: int):
+    pokemon = Pokemon.objects.get(id=id) #select * from pokedex_pokemon where id = id
+    pokemon.delete() #delete from pokedex_pokemon where id = id
+    return redirect('pokedex:index')
