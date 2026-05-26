0ed395b431fab836f086bf247144f4c282f31a10
laboratorio.fs <jhelan.basantes@uisek.edu.ec>
2026-05-26 11:05:01 -0500

    Laboratorio 6

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,10 +1,10 @@
-from multiprocessing import context
-
 from django.http import HttpResponse
 from django.template import loader
 from .models import Pokemon, Trainer
 from django.shortcuts import redirect, render
 from pokedex.forms import PokemonForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     pokemons = Pokemon.objects.all() #Select * from pokedex_pokemon
@@ -31,6 +31,8 @@ def trainer(request, id: int):
     }
     return HttpResponse(template.render(context, request))
 
+
+@login_required
 def add_pokemon(request):
     if request.method == 'POST':
         form = PokemonForm(request.POST, request.FILES)
@@ -52,7 +54,12 @@ def edit_pokemon(request, id: int):
         form = PokemonForm(instance=pokemon)
     return render(request, 'pokemon_form.html', {'form': form})
 
+
+@login_required
 def delete_pokemon(request, id: int):
     pokemon = Pokemon.objects.get(id=id) #select * from pokedex_pokemon where id = id
     pokemon.delete() #delete from pokedex_pokemon where id = id
     return redirect('pokedex:index')
+
+class CustomLoginView(LoginView):
+    template_name = 'login_form.html'
