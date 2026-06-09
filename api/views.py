3bb9cc0c27ee41e9589acdc7b92a6416858d64de
Juan Pablo <juan.andagoya@uisek.edu.ec>
2026-06-09 11:18:27 -0500

    Lab 8

--- api/views.py
+++ api/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from pokedex.models import Pokemon
 from .serializers import PokemonSerializer
+from oauth2_provider.contrib.rest_framework import TokenHasScope, OAuth2Authentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 class PokemonViewSet(viewsets.ModelViewSet):
     queryset = Pokemon.objects.all()
     serializer_class = PokemonSerializer
 
+    authentication_classes = [OAuth2Authentication]
+    required_scopes = ['read']
+
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
+            return [IsAuthenticated()]
+        return [AllowAny()]
 
